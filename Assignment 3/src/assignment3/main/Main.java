0f76e9b53471bb7e9fdb915631c453f5b651638a
Jeppe <jeppestougaardfaber@gmail.com>
2017-10-03 17:47:18 +0200

    sync with assignment 3

--- Assignment 3/src/assignment3/main/Main.java
+++ Assignment 3/src/assignment3/main/Main.java
@@ -1,12 +1,16 @@
 package assignment3.main;
 
-public class Main {
-    public static void main(String[] args) {
 
-        int fieldSize = 2 ;
+public class Main{
+
+    public static void main(String[] args){
+
+        int fieldSize = 25 ;
         int numBombs = 5;
-
         Field theField = new Field(fieldSize, fieldSize, numBombs);
         theField.printFieldToConsole();
+
+
+
     }
 }
