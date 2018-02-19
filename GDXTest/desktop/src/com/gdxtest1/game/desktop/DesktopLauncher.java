80533a5b631c09f0133515d9c1c685a49689a438
Jeppe <jeppestougaardfaber@gmail.com>
2018-02-19 09:30:21 +0100

    continued working on testGDX

--- GDXTest/desktop/src/com/gdxtest1/game/desktop/DesktopLauncher.java
+++ GDXTest/desktop/src/com/gdxtest1/game/desktop/DesktopLauncher.java
@@ -5,8 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gdxtest1.game.GameClass;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GameClass(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "hello world";
+        config.useGL30 = false;
+        config.width = 700;
+        config.height = 700;
+        new LwjglApplication(new GameClass(), config);
+    }
 }
